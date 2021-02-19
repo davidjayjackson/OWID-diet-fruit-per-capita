628f1178f435a371fcb00fb367c5c14105505bf7
davidjayjackson <davidjayjackson@gmail.como>
2021-02-19 17:00:07 -0500

    Created plot of worldwide fruit comsumption per fruit per year 2021-02-19

--- fruit_vegetable_consumption-per-capita.R
+++ fruit_vegetable_consumption-per-capita.R
@@ -41,10 +41,27 @@ ggplot(countries_five) + geom_line(aes(x=Year,y=Fruits,col=Country)) +
   geom_smooth(aes(x=Year, y= Fruits),method="lm")
 
 
-### Plot of US Fruit consumption 
+## Part 3: Fruit  Consumption by Type of Fruit, Year and Country
 
-fruit %>% filter(Country =="United States") %>%
-  ggplot() + geom_line(aes(x=Year,y=Fruits)) +
-  labs(title="US Fruit Consumption: Kg/Year/Person (1960-2017)",
-       y="Kilograms Per Person")
-  
+fruit_type <- read.csv("./DATA/3-fruit-consumption-by-fruit-type.csv")
+colnames(fruit_type) <-c("Country","Code","Year",
+                         "Apples","Bananas","Citrus",
+                         "Dates","Other","Grapefruit",
+                         "Grapes","Lemons","Oranges",
+                         "Pineapples","Plantains")
+fruit_type <- fruit_type %>% select(Country:Dates,Grapefruit:Plantains,Other)
+summary(fruit_type)
+fruit_type_long <- fruit_type %>% pivot_longer(cols = Apples:Other) %>%
+  rename("Fruit" = "name") %>% rename("perCapita" = "value") %>% select(-Code)
+
+### PLot of Fruit Consumption Wroldwide by Fruit Type
+fruit_type_long %>% filter(Country =="World" & Fruit !="Other") %>% 
+  ggplot() + geom_line(aes(x=Year,y=perCapita,col=Fruit,size=0.5)) +
+  labs(title="Fruit Consumption per Person by Fruit and Year",
+       caption = "(Average fruit consumption per person, differentiated by fruit types, measured in Kilograms per Year)",
+       y="Kilograms per Person") + guides(color = guide_legend(override.aes = list(size = 2))) +
+  scale_y_continuous(
+    labels = scales::number_format(accuracy = 0.01,
+                                   decimal.mark = '.')) 
+
+
